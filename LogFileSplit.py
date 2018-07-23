24d1d2d47e910a77274e09303e1d9389425c5d00
gas-snake <lds190@126.com>
2018-07-24 02:30:44 +0800

    22

--- LogFileSplit.py
+++ LogFileSplit.py
@@ -4,9 +4,31 @@ import ofile
 
 LN= "C:\\Users\\Terisa\\Desktop\\python\\iplistall2\\HAJIZ-MC-BA01-FENGSHOULU-(2017_11_08).txt"
 
-LOG=ofile.OpenFile(LN,'f')
-LOGS=LOG.split('#\r\n')
+# LOG=ofile.OpenFile(LN,'f')
+# LOGS=LOG.split('#\r\n')
+# print (type(LOG))
+# print (LOGS)
+# for l in LOGS:
+#     print (l)
+
+LOG=ofile.OpenFile(LN,'l')
+
 print (type(LOG))
+print (LOG)
+print ('=======')
+LOGS = []
+t = []
+for l in LOG:
+    l=l.strip("\r\n")
+    # print (l)
+    
+    # print (t)
+    if l=="#":
+        LOGS.append(t)
+        t=[]
+    t.append(l)
 print (LOGS)
-for l in LOGS:
-    print (l)
+print (LOGS[15])
+print (LOGS[35])
+print (LOGS[75])
+print (LOGS[105])
