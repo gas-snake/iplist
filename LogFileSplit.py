3cc6587560b90ebb2305d66f03b645c0a6ad3f61
gas-snake <lds190@126.com>
2019-09-11 11:08:45 +0800

    jj

--- LogFileSplit.py
+++ LogFileSplit.py
@@ -3,7 +3,7 @@
 import ofile
 
 LN= "C:\\Users\\Terisa\\Desktop\\python\\iplistall2\\HAJIZ-MC-BA01-FENGSHOULU-(2017_11_08).txt"
-
+LN2="C:\\Python\\TEST\\HAJIY-MC-BA01-gaoxinju_170831.log"
 # LOG=ofile.OpenFile(LN,'f')
 # LOGS=LOG.split('#\r\n')
 # print (type(LOG))
@@ -11,7 +11,7 @@ LN= "C:\\Users\\Terisa\\Desktop\\python\\iplistall2\\HAJIZ-MC-BA01-FENGSHOULU-(2
 # for l in LOGS:
 #     print (l)
 
-LOG=ofile.OpenFile(LN,'l')
+LOG=ofile.OpenFile(LN2,'l')
 
 print (type(LOG))
 print (LOG)
@@ -24,10 +24,14 @@ for l in LOG:
     
     # print (t)
     if l=="#":
-        LOGS.append(t)
+        
         t=[]
-    t.append(l)
+    else :
+        t.append(l)
+    LOGS.append(t)
+    
 print (LOGS)
+print ("88888888")
 print (LOGS[15])
 print (LOGS[35])
 print (LOGS[75])
